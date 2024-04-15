3e7c55f2cd53912f63d20ea0ec278ed076c1be47
Din Mušović <93013218+DinMSC@users.noreply.github.com>
2024-04-15 09:43:42 +0200

    added rando raiting

--- connectcartco/src/components/ProductCard.jsx
+++ connectcartco/src/components/ProductCard.jsx
@@ -1,8 +1,17 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { CartContext } from '../context/Cart.Context';
 
 const PorductCard = ({ name, price, description, image }) => {
     const { addToCart } = useContext(CartContext);
+
+    const [discount, setDiscount] = useState(
+        Math.floor(Math.random() * 71) + 10
+    );
+    useEffect(() => {
+        setDiscount(Math.floor(Math.random() * 71) + 10);
+    }, []);
+
+    const originalPrice = (price / (1 - discount / 100)).toFixed(2);
 
     const handleAddToCart = () => {
         addToCart({
@@ -21,7 +30,7 @@ const PorductCard = ({ name, price, description, image }) => {
             >
                 <img className='object-cover' src={image} alt='product image' />
                 <span className='absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white'>
-                    39% OFF
+                    {discount}% OFF
                 </span>
             </a>
             <div className='mt-4 px-5 pb-5'>
@@ -37,11 +46,58 @@ const PorductCard = ({ name, price, description, image }) => {
                             {price}$
                         </span>
                         <span className='text-sm text-slate-900 line-through'>
-                            $699
+                            ${originalPrice}
                         </span>
                     </p>
                     <div className='flex items-center'>
-                        // Your SVGs here...
+                        <svg
+                            aria-hidden='true'
+                            class='h-5 w-5 text-yellow-300'
+                            fill='currentColor'
+                            viewBox='0 0 20 20'
+                            xmlns='http://www.w3.org/2000/svg'
+                        >
+                            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'></path>
+                        </svg>
+                        <svg
+                            aria-hidden='true'
+                            class='h-5 w-5 text-yellow-300'
+                            fill='currentColor'
+                            viewBox='0 0 20 20'
+                            xmlns='http://www.w3.org/2000/svg'
+                        >
+                            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'></path>
+                        </svg>
+                        <svg
+                            aria-hidden='true'
+                            class='h-5 w-5 text-yellow-300'
+                            fill='currentColor'
+                            viewBox='0 0 20 20'
+                            xmlns='http://www.w3.org/2000/svg'
+                        >
+                            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'></path>
+                        </svg>
+                        <svg
+                            aria-hidden='true'
+                            class='h-5 w-5 text-yellow-300'
+                            fill='currentColor'
+                            viewBox='0 0 20 20'
+                            xmlns='http://www.w3.org/2000/svg'
+                        >
+                            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'></path>
+                        </svg>
+                        <svg
+                            aria-hidden='true'
+                            class='h-5 w-5 text-yellow-300'
+                            fill='currentColor'
+                            viewBox='0 0 20 20'
+                            xmlns='http://www.w3.org/2000/svg'
+                        >
+                            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'></path>
+                        </svg>
+                        <span class='mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold'>
+                            5.0
+                        </span>
                     </div>
                 </div>
                 <button
